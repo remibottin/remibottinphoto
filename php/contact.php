d500da25e9e07c21d2b22522efa8cb4c1e861c89
RemiBottin <remibottin12@gmail.com>
2020-05-07 18:07:27 +0200

    re-design

--- php/contact.php
+++ php/contact.php
@@ -1,26 +1,15 @@
 <?php
-$nom=$HTTP_POST_VARS['nom'];
-$mail=$HTTP_POST_VARS['mail'];
-$objet=$HTTP_POST_VARS['objet'];
-$message=$HTTP_POST_VARS['message'];
 
-$headers = "MIME-Version: 1.0\r\n";
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
 
-//////mail and text format
-$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $mailTo = "remibottin12@gmail.com"
+  $headers = "From: ".$mailFrom;
+  $text = "You have received an e-mail from ".$name.".".\n\n".$message;
 
-////exp and response
-$headers .= "From: $nom <$mail>\r\nReply-to : $nom <$mail>\nX-Mailer:PHP";
-
-$subject="$objet";
-$destinataire="remibottin12@gmail.com"; // mon mail
-$body="$message";
-if (mail($destinataire,$subject,$body,$headers)) {
-echo "Votre mail a été envoyé<br>";
-} else {
-echo "Une erreur s'est produite";
+  mail($mailTO, $subject, $txt, $headers);
+  header("Location: index.html?mailsend")
 }
-?></p>
-<p align="center">You are going be redirected automaticaly<br>
-If not, click here: <a href="work.html">ici
-</a></p>
